331ff5d736f08f8d2ea237d89e97b0e028f99394
Ben Weinstein-Raun <b@w-r.me>
2022-10-29 03:32:11 -0700

    remove unnecessary print statements

--- src/main.rs
+++ src/main.rs
@@ -55,65 +55,50 @@ async fn sponsor(mut hole: Wormhole, starting_stage: usize) -> Result<(), Error>
     let mut tarball = vec![];
     let enc = flate2::write::GzEncoder::new(&mut tarball, flate2::Compression::default());
     let mut tar = tar::Builder::new(enc);
+    // (the programs in 'enroll' are in the top-level of the archive)
     tar.append_dir_all(".", "enroll")?;
-    info!("Created tarball");
     drop(tar);
 
     // 2. Send them over
-    // tarball.read_to_end(&mut contents)?;
     hole.send(tarball).await?;
-    info!("Sent tarball");
 
-    // 3. For each initiator program,
+    // 3. For each sponsor program,
     let mut entries = std::fs::read_dir("sponsor")?
         .filter_map(|r| r.ok())
         .collect::<Vec<_>>();
     entries.sort_by_key(|e| e.path());
     for e in entries[starting_stage..].into_iter() {
-        info!("Running cmd {}", e.path().display());
         let metadata = e.metadata()?;
         if metadata.is_file() {
-            info!("cmd is a file; creating command");
             //   a. Execute it
             let child = std::process::Command::new(e.path())
                 .stdout(Stdio::piped())
                 .stderr(Stdio::inherit())
                 .stdin(Stdio::inherit())
                 .spawn()?;
-            info!("spawned command");
             let output = child.wait_with_output()?;
-            info!("got output");
             //   b. Send its output to the remote
             hole.send(output.stdout).await?;
-            info!("sent output");
         }
 
         //   c. Receive any output from the remote
         let output = hole.receive().await?;
-        info!("received output");
-        info!("writing to {}", PathBuf::from("results").join(e.path().file_name().expect("No filename?")).display());
 
         //   d. Save that output
         let mut outfile = std::fs::File::create(PathBuf::from("results").join(e.path().file_name().expect("no filename?")))?;
-        info!("created outfile");
         outfile.write(&output)?;
-        info!("wrote outfile");
     }
     Ok(())
 }
 
-async fn enroll(mut hole: Wormhole, tmpdir: &std::path::Path) -> Result<(), Error> {
-    info!("receiving tarball");
+async fn enroll(mut hole: Wormhole) -> Result<(), Error> {
     let tarball_bytes = hole.receive().await?;
-    info!("received tarball");
     let mut archive = tar::Archive::new(flate2::read::GzDecoder::new(tarball_bytes.as_slice()));
-    archive.unpack(tmpdir)?;
-    info!("unpacked tarball");
-    let mut entries = std::fs::read_dir(tmpdir)?
+    archive.unpack(".")?;
+    let mut entries = std::fs::read_dir(".")?
         .filter_map(|r| r.ok())
         .collect::<Vec<_>>();
     entries.sort_by_key(|e| e.path());
-    info!("got tarball contents: {:?}", entries.iter());
     for e in entries {
         let metadata = e.metadata()?;
         if metadata.is_file() {
@@ -161,31 +146,19 @@ async fn main() -> Result<(), Error> {
 
     match &args.command {
         Command::Enroll { wormhole_code } => {
-            info!("enrolling");
             let dir = TempDir::new()?;
-            info!("made temp dir at {}", dir.path().display());
             std::env::set_current_dir(dir.path())?;
-            info!("Set dir");
             let (welcome, hole) =
                 Wormhole::connect_with_code(APP_CONFIG, Code(wormhole_code.to_string())).await?;
-            info!("opened wormhole");
-            info!("{}", welcome.welcome.unwrap_or(String::new()));
-            enroll(hole, dir.path()).await?;
+            enroll(hole).await?;
         }
-        Command::Sponsor {
-            path,
-            starting_stage,
-        } => {
+        Command::Sponsor { path, starting_stage } => {
             let p: PathBuf = path.as_ref().unwrap_or(&PathBuf::from(".")).to_path_buf();
-            info!("got {:?}", p);
             std::env::set_current_dir(&p)?;
-            info!("set working dir");
             let (welcome, holefuture) = Wormhole::connect_without_code(APP_CONFIG, 10).await?;
-            info!("{}", welcome.welcome.unwrap_or(String::new()));
-            eprintln!("On the enrollee, run:\n\n");
+            eprintln!("On the enrollee, run:\n");
             eprintln!("curl -fsSL https://github.com/benwr/soanm/releases/[determine host type] > soanm && ./soanm {}", welcome.code);
             let hole = holefuture.await?;
-            info!("opened wormhole");
             sponsor(hole, starting_stage.unwrap_or(0)).await?;
         }
     }
